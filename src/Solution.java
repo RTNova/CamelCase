807a8541cc763ff1c25e2c0febd4e78f0949f047
RTNova <120527212+Spidernova@users.noreply.github.com>
2025-03-18 19:16:59 +0000

    Resolución del código

--- src/Solution.java
+++ src/Solution.java
@@ -6,6 +6,18 @@ class Solution{
 
     public static String toCamelCase(String str){
 
-        return str;
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+
+        String[] words = str.split("[-_]");
+        StringBuilder camelCaseString = new StringBuilder(words[0]);
+
+        for (int i = 1; i < words.length; i++) {
+            camelCaseString.append(words[i].substring(0, 1).toUpperCase())
+                    .append(words[i].substring(1));
+        }
+
+        return camelCaseString.toString();
     }
 }
